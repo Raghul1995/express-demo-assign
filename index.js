9b1945748fa500530a8ee5ab596d333f9c8294c5
Raghul Mohan <rahulmohan997@gmail.com>
2023-06-10 00:24:37 +0200

    edit again

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res){
   res.send('Hello World continous deployment for GKE!!!!!');
 });
 
-/* istanbul ignore next */
+
 if (!module.parent) {
   app.listen(3000);
   console.log('Express started on port 3000');
